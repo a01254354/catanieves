ff3af78135c1ebf9772bc1e156834337e165422b
a01254354 <112566450+a01254354@users.noreply.github.com>
2022-08-31 07:37:56 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Asleep)
+    basic.showIcon(IconNames.Sad)
+})
